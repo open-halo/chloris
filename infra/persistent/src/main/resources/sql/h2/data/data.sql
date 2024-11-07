273cc004c20c8f9b3573a8386c20ee38bf5973ca
wangwei <wangwei@B-T7HH9PHX-1908.local>
2024-11-07 14:59:37 +0800

    udpate sql

--- infra/persistent/src/main/resources/sql/h2/data/data.sql
+++ infra/persistent/src/main/resources/sql/h2/data/data.sql
@@ -1,7 +1,9 @@
-INSERT INTO `endpoint` (id, http_method, http_url, sql_template) VALUES (1, 'GET', '/api/data_v1', 'select * from `author` where `id` = #{id}');
+INSERT INTO `endpoint` (id, http_method, http_url, sql_template) VALUES (1, 'GET', '/api/data_v1', 'select id, name from `author` where `id` = #{id}');
+INSERT INTO `endpoint` (id, http_method, http_url, sql_template) VALUES (2, 'GET', '/api/data_v2', 'select email from `author` where `name` = #{name}');
 
-INSERT INTO `author` (id, gender, name, email) VALUES (101, 'F', 'Joshua Bloch ', 'joshua.bloch@example.com');
-INSERT INTO `author` (id, gender, name, email) VALUES (102, 'F', 'Scott Meyers ', 'scott.meyers@example.com');
+
+INSERT INTO `author` (id, gender, name, email) VALUES (101, 'F', 'Joshua Bloch', 'joshua.bloch@example.com');
+INSERT INTO `author` (id, gender, name, email) VALUES (102, 'F', 'Scott Meyers', 'scott.meyers@example.com');
 
 
 
